d54cab626d745e1c293cfacf59e4721a21b3db67
PillowSky <hyzgog@gmail.com>
2016-08-05 18:38:25 +0800

    assign default value

--- ss-who/ss-who.pl
+++ ss-who/ss-who.pl
@@ -3,15 +3,25 @@
 use strict;
 use warnings;
 
-if ($#ARGV != 1) {
-	print "Usage: perl $0 <filename> <date>\n";
+if ($#ARGV > 1) {
+	print "Usage: perl $0 [filename] [date]\n";
 	exit;
+} else {
+	if ($#ARGV == -1) {
+		$ARGV[0] = "/var/log/syslog";
+	}
+	if ($#ARGV == 0) {
+		my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime();
+		$ARGV[1] = sprintf("%d-%02d-%02d", $year + 1900, $mon + 1, $mday);	
+	}
 }
+
 
 my $filename = $ARGV[0];
 my $date = $ARGV[1];
-my %database;
+print("File: $filename Date: $date\n\n");
 
+my %database;
 my $in_regex = qr/^.+ss-server.+($date \d{2}:\d{2}:\d{2}).+accept a connection from (\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})$/;
 my $out_regex = qr/^.+connect to: (.+)$/;
 
